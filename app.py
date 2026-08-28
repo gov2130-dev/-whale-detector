7ea5ae2a5c19d77c113d7c8206a5c9ebfb2bd71c
gov2130-dev <gov2130@gmail.com>
2026-08-28 10:30:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import streamlit as st, yfinance as yf, requests, json, os, time
-from datetime import datetime
+import streamlit as st, yfinance as yf, requests, json, os
+from datetime import import datetime
 import pytz
 
-BOT_TOKEN="8594574378:AAEqZ3fbmEDrnnwgwW3yJIwH0kYNIneY9HY"
-CHAT_ID="13889370"
+BOT_TOKEN = st.secrets["BOT_TOKEN"]
+CHAT_ID = st.secrets["CHAT_ID"]
+SENT_FILE="sent_today.json"
 SENT_FILE="sent_today.json"
 RIYADH = pytz.timezone('Asia/Riyadh')
 NY = pytz.timezone('America/New_York')
