96a8481d4b3c5a7bd41e7ad2ae4eb283c9d0de9c
gov2130-dev <gov2130@gmail.com>
2026-08-28 10:32:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st, yfinance as yf, requests, json, os
-from datetime import import datetime
+from datetime import datetime
 import pytz
 
 BOT_TOKEN = st.secrets["BOT_TOKEN"]
 CHAT_ID = st.secrets["CHAT_ID"]
+SENT_FILE="sent_today.json"
 SENT_FILE="sent_today.json"
 SENT_FILE="sent_today.json"
 RIYADH = pytz.timezone('Asia/Riyadh')
